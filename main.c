7e4250c2a0d16ac9ac9fa5c802f664cd599e91d1
syrfnajwaa <said_nizamudin@smltech.co.id>
2024-11-16 13:38:58 +0700

    tampilan awal + fix login

--- main.c
+++ main.c
@@ -2,7 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
+void tampilanAwal() {
+    printf("============================================\n");
+    printf("=                                          =\n");
+    printf("=             SELAMAT DATANG               =\n");
+    printf("=                                          =\n");
+    printf("============================================\n");
+    printf("=      WHO WANTS TO BE A MILLIONAIRE       =\n");
+    printf("============================================\n");
+    printf("=                                          =\n");
+    printf("= Tekan enter untuk melanjutkan registrasi =\n");
+    printf("=                                          =\n");
+    printf("============================================\n");
+    getchar();
+
+}
+
 int main () {
+    tampilanAwal();
+
+
     FILE *fpw = fopen("database/login.bin","wb");
     if (fpw == NULL) {
         printf("hallo");
@@ -17,7 +36,7 @@ int main () {
     fgets(username,sizeof(username),stdin);
     username[strcspn(username, "\n")] = '\0';
     
-    printf("masukkan password anda : ");
+    printf("masukkan password anda :");
     fgets(password,sizeof(password),stdin);
     password[strcspn(password, "\n")] = '\0';
 
@@ -38,6 +57,44 @@ int main () {
     fread(username,sizeof(char),sizeof(username)/sizeof(char),fpr);
 
     fclose(fpr);
-    printf("%s",username);
+    char *string[3];
+    char user[20], pass[20];
+    int ctrl = 0;
+
+    string[0] = strtok(username,"@");
+    while(string[ctrl++] != NULL) {
+        string[ctrl] = strtok(NULL,"@");
+    }
+
+    strcpy(user, string[0]);
+    strcpy(pass, string[1]);
+
+    printf("\n\n=============================================\n");
+    printf("=         Registrasi Anda Berhasil          =\n");
+    printf("=============================================\n");
+    printf("= Silahkan Log in untuk memulai permainan!  =\n");
+    printf("=============================================\n");
+
+    
+    char userLogin[10], passLogin[10];
+
+    printf("\nUsername:");
+    fgets(userLogin,sizeof(userLogin),stdin);
+    userLogin[strcspn(userLogin, "\n")] = '\0';
+
+    printf("Password:");
+    fgets(passLogin,sizeof(passLogin),stdin);
+    passLogin[strcspn(passLogin, "\n")] = '\0';
+    
+    if((strcmp(userLogin, user)) == 0 && (strcmp(passLogin, pass) == 0)) {
+        printf("SELAMAT ANDA BERHASIL LOGIN!\n");
+    } else {
+        printf("AND LOGIN GAGAL!\n");
+    }
+    printf("=============================================\n");
+    printf("=  Tekan enter untuk melanjutkan permainan  =\n");
+    printf("=============================================\n");
+    getchar();
+
     return 0;
 }
